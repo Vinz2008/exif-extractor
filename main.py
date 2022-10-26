9a5171a06dc603b93e1a6406773af9c98a4946f0
Vinz2008 <vincentbidarddelanoe@gmail.com>
2022-10-26 17:11:14 +0200

    added requirements.txt

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import exifread
 path = input("what is the path of the file do you want to use ? ")
-f = open(path, 'rb')
-tags = exifread.process_file(f)
+with open(path, 'rb') as f:
+    tags = exifread.process_file(f, strict=True)
 f.close()
 
 g = open("exif.txt", mode = "w")
 tags_str = str(tags) 
 slash = "\\"
-tags_replaced = tags_str.replace(slash,"\n")
-tags_replaced2 = tags_replaced.replace(",", "\n")
-g.write(str(tags_replaced2))
+tags_str = tags_str.replace(slash,"\n")
+tags_str = tags_str.replace(",", "\n")
+g.write(str(tags_str))
 g.close()
-print(tags_replaced)
+print(tags_str)
